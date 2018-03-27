6c294ce4bbfb78ffa0852626301d98496b1a018a
Robin LANGEOIRE <rlangeoi@e3r2p3.42.fr>
2018-03-27 15:51:01 +0200

    lol

--- includes/vm.h
+++ includes/vm.h
@@ -6,12 +6,17 @@
 /*   By: rlangeoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/27 14:55:05 by rlangeoi          #+#    #+#             */
-/*   Updated: 2018/03/27 15:40:43 by rlangeoi         ###   ########.fr       */
+/*   Updated: 2018/03/27 15:50:53 by rlangeoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		VM_H
 # define	VM_H
+
+# include "op.h"
+
+# define	REDUCED_CTD		(CYCLE_TO_DIE - (CYCLE_DELTA * data->cycle_reductions))
+# define	CYCLE_TO_CHECK	(data->cycles % REDUCED_CTD)
 
 typedef struct	s_vm
 {
@@ -22,6 +27,7 @@ typedef struct	s_vm
 	t_list	*players;
 	int		cycles;
 	int		players_alive;
+	int		cycle_reduction;
 }
 
 #endif
