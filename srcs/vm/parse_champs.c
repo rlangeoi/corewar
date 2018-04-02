2eece4be1d5465885e04545f48638d1ff792c765
rlangeoi <rlangeoi@student.42.fr>
2018-04-02 11:14:47 +0200

    parsing toujours

--- srcs/vm/parse_champs.c
+++ srcs/vm/parse_champs.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/01 18:32:26 by                   #+#    #+#             */
-/*   Updated: 2018/04/02 10:55:42 by                  ###   ########.fr       */
+/*   Updated: 2018/04/02 11:13:41 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,11 @@ void	ft_parse_headers(t_vm *data, int fd)
 		exit_error(ERR_MALLOC, NULL);
 	if ((i = read(fd, (void *)(header), sizeof(header_t))) == -1)
 		exit_error(ERR_READ, NULL);
-//	if (i != sizeof(header))
-//		exit_error(ERR_HEADER, header->prog_name);
+	if (i != sizeof(header_t))
+		exit_error(ERR_HEADER, header->prog_name);
 	header->magic = switch_endianness(header->magic);
 	header->prog_size = switch_endianness(header->prog_size);
+	ft_printf("i:%d\n, sizeof:%d\n, magic:%d\n, corewarmagic:%d\n, sizeof:%d\n", i, sizeof(header_t), header->magic, COREWAR_EXEC_MAGIC, sizeof(header));
 	if (!(new = ft_lstnew(((header_t*)header), sizeof(header_t))))
 		exit_error(ERR_MALLOC, NULL);
 	ft_lstadd_end(&(data->headers), new);
