0e86de12e6787513b1582ce2b013e9d9b257cb6d
Robin LANGEOIRE <rlangeoi@e3r13p3.42.fr>
2018-04-06 20:09:24 +0200

    norme

--- includes/op.h
+++ includes/op.h
@@ -6,7 +6,7 @@
 /*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2018/04/05 16:51:48 by                  ###   ########.fr       */
+/*   Updated: 2018/04/06 20:08:33 by rlangeoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@
 */
 
 #ifndef		OP_H
-# define	OP_H
-# define	IND_SIZE			2
-# define	REG_SIZE			4
-# define	DIR_SIZE			REG_SIZE
+#define		OP_H
 
+#define	IND_SIZE	2
+#define	REG_SIZE	4
+#define	DIR_SIZE	REG_SIZE
 
-# define REG_CODE				1
-# define DIR_CODE				2
-# define IND_CODE				3
-
+#define	REG_CODE	1
+#define	DIR_CODE	2
+#define	IND_CODE	3
 
 # define MAX_ARGS_NUMBER			4
 # define MAX_PLAYERS				4
@@ -50,20 +49,12 @@
 # define NBR_LIVE				21
 # define MAX_CHECKS				10
 
-/*
-**
-*/
-
-typedef char	t_arg_type;
+typedef char		t_arg_type;
 
 # define T_REG					1
 # define T_DIR					2
 # define T_IND					4
 # define T_LAB					8
-
-/*
-**
-*/
 
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
@@ -71,9 +62,10 @@ typedef char	t_arg_type;
 
 typedef struct		header_s
 {
-  unsigned int		magic;
-  char				prog_name[PROG_NAME_LENGTH + 1];
-  unsigned int		prog_size;
-  char				comment[COMMENT_LENGTH + 1];
+	unsigned int	magic;
+	char			prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int	prog_size;
+	char			comment[COMMENT_LENGTH + 1];
 }					header_t;
+
 #endif
